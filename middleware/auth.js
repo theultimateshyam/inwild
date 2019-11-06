213db06ca44b4a0cc31fc37cf6363494720fa0c1
ultimate-wizard <shyamasv.a@gmail.com>
2019-11-06 23:20:35 +0530

    token auth middleware, res is now sent as JSON

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+function auth(req, res, next) {
+	const token = req.header('x-auth-token');
+	// user tried brute force stop him right here
+	if (!token)
+		return res
+			.status(401)
+			.send({ error: 'Acess denied. No token provided.' });
+	try {
+		const decoded = jwt.verify(token, process.env.jwtPrivateKey);
+		//populate user details
+		req.user = decoded;
+		next();
+	} catch (ex) {
+		// give user another chance to login
+		res.status(400).send({ error: 'Invalid token' });
+	}
+}
+
+module.exports = auth;
--- routes/users.js
+++ routes/users.js
@@ -2,35 +2,37 @@ const router = require('express').Router();
 const _ = require('lodash');
 const { User, validateUser } = require('../models/user');
 const bcrypt = require('bcrypt');
-
-router.get('/', async (req, res) => {
-    const users = await User.find();
-    res.send(users);
+const auth = require('../middleware/auth');
+router.get('/', auth, async (req, res) => {
+	const users = await User.find();
+	res.send(users);
 });
 
 router.post('/', async (req, res) => {
-    const { error } = validateUser(req.body);
+	const { error } = validateUser(req.body);
 
-    if (error) return res.status(400).send(error.details[0].message);
+	if (error) return res.status(400).send({ error: error.details[0].message });
 
-    let user = await User.findOne({ email: req.body.email });
+	let user = await User.findOne({ email: req.body.email });
 
-    if (user) return res.status(400).send('User already exists ');
+	if (user) return res.status(400).send({ error: 'User already exists ' });
 
-    const salt = await bcrypt.genSalt(10);
-    const hashed_password = await bcrypt.hash(req.body.password, salt);
+	const salt = await bcrypt.genSalt(10);
+	const hashed_password = await bcrypt.hash(req.body.password, salt);
 
-    const newuser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: hashed_password,
-        username: req.body.username,
-        designation: req.body.designation
-    });
-    await newuser.save();
+	const newuser = new User({
+		name: req.body.name,
+		email: req.body.email,
+		password: hashed_password,
+		username: req.body.username,
+		designation: req.body.designation
+	});
+	await newuser.save();
 
-    const token = newuser.generateAuthToken();
-    res.header('x-auth-token', token).send(_.pick(newuser, ['_id', 'isAdmin','name']));
+	const token = newuser.generateAuthToken();
+	res.header('x-auth-token', token).send(
+		_.pick(newuser, ['_id', 'isAdmin', 'name'])
+	);
 });
 
 module.exports = router;
