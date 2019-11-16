7db5fd59808588138deb969de7448a4f08b6857b
devSuchit <suchit9903@gmail.com>
2019-11-16 16:08:14 +0530

    ui changes

--- public/js/formcontrol.js
+++ public/js/formcontrol.js
@@ -1,11 +1,13 @@
 function openForm1() {
 	document.getElementById('right-container1').style.display = 'none';
+	document.getElementById('right-container3').style.display = 'none';
 	document.getElementById('right-container2').style.display = 'flex';
 }
 
 function openForm2() {
 	document.getElementById('right-container2').style.display = 'none';
 	document.getElementById('right-container3').style.display = 'flex';
+	$('#Errormessage').html("");
 }
 
 function openForm3() {
